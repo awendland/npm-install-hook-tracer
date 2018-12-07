a02502ee1ae0ce33cc92ddc5d1ac8dcc6e6171ac
Alex Wendland <me@alexwendland.com>
2018-12-07 11:49:57 -0500

    tracer: Use npm-bundle as a module

--- tracer/src/index.ts
+++ tracer/src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import shell from 'shelljs'
+import { statSync } from 'fs'
 import path from 'path'
 import { performance } from 'perf_hooks'
 import * as lib from './lib'
@@ -25,7 +26,8 @@ const msToSec2 = (ms: number) => (ms / 1000).toFixed(2)
   process.stderr.write(`Retrieving "${packageName}" `)
   const downloadStart = performance.now()
   const {packageFile, extractedFolder} = await lib.pullPackage(packageName)
-  process.stderr.write(`time[${msToSec2(performance.now() - downloadStart)}s]\n`)
+  process.stderr.write(`time[${msToSec2(performance.now() - downloadStart)}s] `)
+  process.stderr.write(`size[${(statSync(packageFile).size / 1024).toFixed(2)}KiB]\n`)
   argv.traceDir = argv.traceDir || argv.o || `traces/${packageFile.replace('.tgz', '')}`
   console.error(`Analyzing "${packageFile}"`)
   const packageJson = require(path.resolve(extractedFolder, 'package.json'))
@@ -43,7 +45,7 @@ const msToSec2 = (ms: number) => (ms / 1000).toFixed(2)
     const {traceFiles, stdout, stderr, runtime} =
       await lib.straceScript(path.resolve(argv.traceDir, `${hook}`), script, extractedFolder)
     const traceSize = traceFiles.reduce((sum, stat) => sum + stat.size, 0)
-    process.stderr.write(`traces[${traceFiles.length}] size[${traceSize / 1024}KiB] time[${msToSec2(runtime)}s]\n`)
+    process.stderr.write(`traces[${traceFiles.length}] size[${(traceSize / 1024).toFixed(2)}KiB] time[${msToSec2(runtime)}s]\n`)
   }
 })().catch(e => {
   console.error(e)
